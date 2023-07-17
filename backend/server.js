7506548a6ff38cc005d60c9c9745581a1135bf8b
FaroukChalghoumi <farouk.chalghoumi@esprit.tn>
2023-07-17 09:10:31 +0100

    added middleware to show path and method request

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,16 @@ let PORT = 4000;
 //express app 
 const app = express()
 
+app.use((req,res,next) => {
+    console.log(req.path,req.method)
+    next()
+})
+
+app.get('/' , (req,res) => {
+    res.json({msg: 'welcome to the app'})
+})
+
+
 //listen to port:
 app.listen(PORT , () => {
     console.log('Listening to Port 4000')
